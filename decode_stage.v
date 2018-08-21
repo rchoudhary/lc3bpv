429aeb253398afad4cd08e6d18a5108795fadfd3
Rutvik Choudhary <rchoudhary11235@gmail.com>
2018-08-21 00:29:03 -0500

    Forgot to add some outputs to decode stage and misnamed some signals

--- decode_stage.v
+++ decode_stage.v
@@ -17,7 +17,7 @@ module DecodeStage(
     input v_agex_ld_reg,
     input v_mem_ld_reg,
     input v_sr_ld_reg,
-    input [2:0] agex_drid,
+    input [2:0] agex_drid_old,
     input [2:0] mem_drid,
     input [2:0] sr_drid,
     input [15:0] sr_reg_data,
@@ -28,19 +28,22 @@ module DecodeStage(
     output v_de_br_stall,
     output dep_stall,
     output ld_agex,
-    output agex_v,
-    output [19:0] agex_cc
+    output [15:0] agex_sr1,
+    output [15:0] agex_sr2,
+    output [2:0] agex_drid_new,
+    output [19:0] agex_cs,
+    output agex_v
     );
 
-    wire cs_addr = {de_ir[15:11], de_ir[5]};
-    wire [22:0] de_cc;
+    wire [5:0] cs_addr = {de_ir[15:11], de_ir[5]};
+    wire [22:0] de_cs;
 
     ControlStore CS(
         //Inputs
         .clk(mem_clk),
         .addr(cs_addr),
         // Outputs
-        .csBits(de_cc)
+        .csBits(de_cs)
     );
 
     wire sr2_id_mux = de_ir[13];
@@ -55,28 +58,31 @@ module DecodeStage(
         .sr2(sr2),
         .dr(sr_drid),
         .dataIn(sr_reg_data),
+        .we(v_sr_ld_reg),
         // Outputs
         .sr1Out(sr1_data),
         .sr2Out(sr2_data)
     );
 
-    wire de_br_stall = de_cc[7];
+    wire de_br_stall = de_cs[7];
     assign v_de_br_stall = de_br_stall & de_v;
 
-    wire dr_mux = de_cc[20];
+    wire dr_mux = de_cs[20];
     wire [2:0] dr = (dr_mux == 0) ? de_ir[11:9] : 3'd7; 
 
-    wire sr1_needed = de_cc[22];
-    wire sr2_needed = de_cc[21];
-    wire br_op = de_cc[10];
-    wire sr1_dep_stall = sr1_needed & ((v_agex_ld_reg & agex_drid == sr1) | (v_mem_ld_reg & mem_drid == sr1) | (v_sr_ld_reg & sr_drid == sr1));
-    wire sr2_dep_stall = sr2_needed & ((v_agex_ld_reg & agex_drid == sr2) | (v_mem_ld_reg & mem_drid == sr2) | (v_sr_ld_reg & sr_drid == sr2));
+    wire sr1_needed = de_cs[22];
+    wire sr2_needed = de_cs[21];
+    wire br_op = de_cs[10];
+    wire sr1_dep_stall = sr1_needed & ((v_agex_ld_reg & agex_drid_old == sr1) | (v_mem_ld_reg & mem_drid == sr1) | (v_sr_ld_reg & sr_drid == sr1));
+    wire sr2_dep_stall = sr2_needed & ((v_agex_ld_reg & agex_drid_old == sr2) | (v_mem_ld_reg & mem_drid == sr2) | (v_sr_ld_reg & sr_drid == sr2));
     wire br_dep_stall = br_op & (v_agex_ld_cc | v_mem_ld_cc | v_sr_ld_cc);
     assign dep_stall = de_v & (sr1_dep_stall | sr2_dep_stall | br_dep_stall);
 
     assign ld_agex = ~mem_stall;
+    assign agex_sr1 = sr1_data;
+    assign agex_sr2 = sr2_data;
+    assign agex_drid_new = dr;
+    assign agex_cs = de_cs[19:0];
     assign agex_v = de_v & ~dep_stall;
-
-    assign agex_cc = de_cc[19:0];
 
 endmodule
